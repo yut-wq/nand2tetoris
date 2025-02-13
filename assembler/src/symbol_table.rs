bed41c7bf3076110c074fa7eb58c9ae6b0f60cce
yut-wq <println856@gmail.com>
2025-02-13 22:45:06 +0900

    シンボルテーブル初期化時に定義済みシンボルを追加する

--- assembler/src/symbol_table.rs
+++ assembler/src/symbol_table.rs
@@ -4,7 +4,33 @@ pub struct SymbolTable(HashMap<String, u16>);
 
 impl SymbolTable {
     pub fn new() -> Self {
-        SymbolTable(HashMap::new())
+        // 定義済みシンボルを格納する
+        let table = HashMap::from([
+            ("R0".to_string(), 0),
+            ("R1".to_string(), 1),
+            ("R2".to_string(), 2),
+            ("R3".to_string(), 3),
+            ("R4".to_string(), 4),
+            ("R5".to_string(), 5),
+            ("R6".to_string(), 6),
+            ("R7".to_string(), 7),
+            ("R8".to_string(), 8),
+            ("R9".to_string(), 9),
+            ("R10".to_string(), 10),
+            ("R11".to_string(), 11),
+            ("R12".to_string(), 12),
+            ("R13".to_string(), 13),
+            ("R14".to_string(), 14),
+            ("R15".to_string(), 15),
+            ("SP".to_string(), 0),
+            ("LCL".to_string(), 1),
+            ("ARG".to_string(), 2),
+            ("THIS".to_string(), 3),
+            ("THAT".to_string(), 4),
+            ("SCREEN".to_string(), 16384),
+            ("KBD".to_string(), 24576),
+        ]);
+        SymbolTable(table)
     }
 
     pub fn add_entry(&mut self, symbol: String, address: u16) {
