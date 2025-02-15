fe89e0f6835ecfea1fb153ffce62bbb445f32d54
yut-wq <println856@gmail.com>
2025-02-15 13:46:28 +0900

    利用可能な変数テーブルを作成

--- assembler/src/lib.rs
+++ assembler/src/lib.rs
@@ -1,7 +1,7 @@
 use code::Code;
 use parser::Parser;
 use regex::Regex;
-use std::{fs::File, io::Write};
+use std::{collections::HashMap, fs::File, io::Write, iter::zip};
 use symbol_table::SymbolTable;
 
 mod code;
@@ -17,32 +17,40 @@ pub fn run(file_name: &str) {
     let file_name_base = file_name_base[1].to_string();
     let mut file = File::create(format!("{}.hack", file_name_base)).unwrap();
 
+    // 変数で使用可能なアドレス
+    let keys = 16..0b0111_1111_1111_1111_u16;
+    let values = [true; 0xffff];
+    let key_value_pair = keys.zip(values);
+    let mut available_address: HashMap<u16, bool> = key_value_pair.collect();
+
     // 1パス目
     let mut parser = Parser::new(file_name);
     let mut symbol_table = SymbolTable::new();
     let mut line_count = 0;
     while parser.has_more_line() {
         parser.advance();
+        // println!("{:#?}", parser);
 
         let instruction_type = parser.instruction_type();
         match instruction_type {
-            parser::InstructionType::AInstruction => {
-                line_count += 1;
-            }
-            parser::InstructionType::CInstruction => {
-                line_count += 1;
-            }
+            parser::InstructionType::AInstruction => {}
+            parser::InstructionType::CInstruction => {}
             parser::InstructionType::LInstruction => {
                 let symbol = parser.symbol();
-                symbol_table.add_entry(symbol, line_count + 1);
+                let address = line_count;
+                symbol_table.add_entry(symbol, address);
+                // 使用アドレスを使用不可にする
+                available_address.remove(&address);
             }
         }
+        line_count += 1;
     }
+    println!("{:#?}", symbol_table);
 
     // 2パス目
     let mut parser = Parser::new(file_name);
     let mut bin_codes = String::new();
-    let mut variable_address = 16;
+    let mut min_address = 16;
     while parser.has_more_line() {
         parser.advance();
         // println!("{:#?}", parser);
@@ -58,15 +66,24 @@ pub fn run(file_name: &str) {
                         bin_codes.push_str(&bin_code);
                     }
                     Err(_) => {
+                        println!("{}", symbol);
+                        // if (symbol == "sys") {
+                        //     println!("{:#?}", parser);
+                        // }
                         if symbol_table.contains(&symbol) {
                             let symbol = symbol_table.get_address(&symbol);
                             let bin_code = format!("{:016b}\n", symbol);
                             bin_codes.push_str(&bin_code);
                         } else {
                             // シンボルテーブルに追加する
-                            symbol_table.add_entry(symbol, variable_address);
-                            let symbol = variable_address;
-                            variable_address += 1;
+
+                            // 利用可能な最小アドレスを取得する
+                            while !available_address.contains_key(&min_address) {
+                                min_address += 1;
+                            }
+                            symbol_table.add_entry(symbol, min_address);
+                            let symbol = min_address;
+                            available_address.remove(&min_address);
                             let bin_code = format!("{:016b}\n", symbol);
                             bin_codes.push_str(&bin_code);
                         }
